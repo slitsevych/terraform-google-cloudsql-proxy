546c84bcc8936968fd1918d6f2ea67dd7da0a348
Stepan Litsevych <s.litsevych@aimprosoft.com>
2025-02-10 20:22:24 +0200

    added provisioning model

--- main.tf
+++ main.tf
@@ -75,10 +75,11 @@ resource "google_compute_instance" "main" {
   machine_type = var.vm_machine_type
   zone         = var.vm_zone
 
-  provisioning_model = var.provisioning_model
-  preemptible        = var.provisioning_model == "SPOT" ? true : false
-  automatic_restart  = var.provisioning_model == "SPOT" ? false : true
-
+  scheduling {
+    provisioning_model = var.provisioning_model
+    preemptible        = var.provisioning_model == "SPOT" ? true : false
+    automatic_restart  = var.provisioning_model == "SPOT" ? false : true
+  }
   allow_stopping_for_update = true
 
   boot_disk {
