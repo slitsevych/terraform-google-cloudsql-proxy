82aad46e361fa6c1f7658fd7eee29c1e61cb1147
Stepan Litsevych <s.licevich@gmail.com>
2025-09-17 12:18:10 +0300

    updated versions

--- main.tf
+++ main.tf
@@ -23,9 +23,9 @@ resource "google_project_iam_member" "cloudsql_instance_client_role_to_main_serv
 }
 
 module "gce_container_sqlproxy" {
-  source  = "terraform-google-modules/container-vm/google"
-  version = "git::https://github.com/terraform-google-modules/terraform-google-container-vm.git?ref=main"
+  source = "git::https://github.com/terraform-google-modules/terraform-google-container-vm.git?ref=main"
 
+  # source  = "terraform-google-modules/container-vm/google"
   # version = "~> 3.2"
 
   cos_image_family = var.cos_image_family
